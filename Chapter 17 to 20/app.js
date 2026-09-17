a08abd8081e1e967d79aec06aec7d22c9c58069e
NuflahNawal <nuflahnawalnoor559@gmail.com>
2026-09-18 00:27:10 +0500

    JS

---
+++ Chapter 17 to 20/app.js
@@ -0,0 +1,52 @@
+// QUESTION 1 , 2
+var arr = [[0 , 1 , 2 , 3], [1 , 0 , 1 , 2], [2 , 1 , 0 , 1]]
+document.write(arr[0][0] , arr [0][1] , arr [0][2] , arr [0][3] + "<br>")
+document.write(arr[1][0] , arr [1][1] , arr [1][2] , arr [1][3] + "<br>")
+document.write(arr[2][0] , arr [2][1] , arr [2][2] , arr [2][3] + "<br> <br>")
+
+// QUESTION 3
+for (var i=1; i<=10; i++){
+    document.write(i + "<br>")
+}
+
+// QUESTION 4
+// var num = prompt ("Enter a number for table!") || 5
+// var multiple = prompt ("Enter a number for multiples") ||10
+
+// for (var i=1; i<= multiple; i++){
+//     document.write(`${num} x ${i} = ${num*i} <br>`)
+// }
+
+// QUESTION 5
+var arr = [" Apple" , "Banana" , "Mango" , "Orange" , "Strawberry"]
+document.write(`${arr [0]} <br> ${arr [1]} <br> ${arr[2]} <br> ${arr [3]} <br> ${arr [4]} <br><br>`)
+
+
+// QUESTION  6
+// COUNTING
+for(var i=1; i<=10; i++){
+document.write(i + "<br>")
+}
+
+// REVERSE COUNTING
+for( var i=10; i>=0; i--){
+document.write(i + "<br>")
+}
+
+// EVEN
+for( var i=0; i<=10; i+=2){
+document.write(i )
+}
+
+// ODD
+for( var i=0; i<=10; i++){
+    i++
+document.write("<br>" + i )
+}
+
+// SERIES
+for( var i=2; i<=20; i+=2){
+document.write("<br>" + i +  "K" )
+}
+
+// QUESTION 7
